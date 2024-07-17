e1bfac263e2c1db66b9e7666eec3d713940db782
Quide1 <ezequielr.dev@gmail.com>
2024-07-17 02:57:24 -0300

    test 2

--- src/pages/TestPageWithParam.tsx
+++ src/pages/TestPageWithParam.tsx
@@ -1,12 +1,39 @@
-import { useParams } from "react-router-dom";
+import { getWeaponById } from "@/services/weapons";
+import { useEffect } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 function TestPageWithParam() {
   const { weapon } = useParams();
-  console.log(weapon);
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const weaponUuid = searchParams.get('idWeapon');
+        
+        if (!weaponUuid) {
+          navigate('/');
+          return;
+        }
+
+        const responseFetch = await getWeaponById(weaponUuid);
+        console.log(responseFetch); // Aquí puedes manejar la respuesta de la API
+
+      } catch (error) {
+        console.error('Error fetching weapon:', error);
+        navigate('/'); 
+      }
+    };
+
+    fetchData();
+
+  }, [searchParams, navigate]);
+
   return (
     <div>
       <h1>hola {weapon}</h1>
-      <p>Este parrafo tendria que hablar sobre la weapon {weapon}</p>
+      <p>Este párrafo debería hablar sobre la weapon {weapon}</p>
     </div>
   );
 }
