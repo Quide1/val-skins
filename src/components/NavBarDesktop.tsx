d266e9caaa3f9cd01255314e92f3142559786af5
Quide1 <ezequielr.dev@gmail.com>
2024-07-17 16:37:18 -0300

    change anchor to link react router dom

--- src/components/NavBarDesktop.tsx
+++ src/components/NavBarDesktop.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import SheetMenu from "./NavBarContent";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Link } from "react-router-dom";
 
 function NavBarDesktop() {
   const [isLargeScreen, setIsLargeScreen] = useState(false);
@@ -29,14 +30,14 @@ function NavBarDesktop() {
       {isLargeScreen && (
           <aside className="bg-slate-800 h-full w-[260px] transform transition-transform duration-300 ease-in-out border-r-2 border-blue-600  flex-col p-0">
             <ScrollArea className="h-full w-full p-4">
-              <a href="/" className="w-full">
+              <Link to="/" className="w-full">
                 <div className="flex flex-row justify-center text-lg gap-4 items-center h-10">
                   <h2 className="font-bold text-red-600">Vaskins</h2>
                   <picture className="max-w-8">
                     <img src="/ValorantLogo.png" alt="valorant logo" />
                   </picture>
                 </div>
-              </a>
+              </Link>
               <section>
                 <ul className="mt-2 mb-2 text-gray-400 flex flex-col">
                   <SheetMenu />
