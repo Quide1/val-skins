a159cb3bdc8a4def4068759edf82117d72661d7d
Quide1 <ezequielr.dev@gmail.com>
2024-07-31 02:34:19 -0300

    use api for map components

--- src/pages/WeaponPage.tsx
+++ src/pages/WeaponPage.tsx
@@ -1,37 +1,50 @@
-// import WeaponCard from "@/components/WeaponCard";
-// import { getWeaponById } from "@/services/weapons";
-// import { useEffect } from "react";
+import { getWeaponById } from "@/services/weapons";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import weaponSkinOdin from '@/data/weaponSkinMokup.json'
 import WeaponSkinCard from "@/components/WeaponSkinCard";
+import { type Skin } from "@/types/weaponType";
+
 function WeaponPage() {
   const { weapon } = useParams();
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const [weaponSkins, setWeaponSkins] = useState<Skin[] | null>(null);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const weaponUuid = searchParams.get("idWeapon");
-  //       if (!weaponUuid) {
-  //         navigate("/");
-  //         return;
-  //       }
-  //       const responseFetch = await getWeaponById(weaponUuid);
-  //     } catch (error) {
-  //       console.error("Error fetching weapon:", error);
-  //       navigate("/");
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchData = async () => {
+      const weaponUuid = searchParams.get("idWeapon");
+      if (!weaponUuid) {
+        navigate("/");
+        return;
+      }
+      try {
+        const response = await getWeaponById(weaponUuid);
+        if (response && response.data && response.data.skins) {
+          setWeaponSkins(response.data.skins);
+        } else {
+          console.error("No skins found for weapon.");
+          navigate("/");
+        }
+      } catch (error) {
+        console.error("Error fetching weapon:", error);
+        navigate("/");
+      }
+    };
 
-  //   fetchData();
-  // }, [searchParams, navigate]);
+    fetchData();
+  }, [searchParams, navigate]);
 
   return (
-    <div>
-      <h1>Pagina del arma : {weapon}</h1>
-      <p>tiene el uuid : {searchParams.get('idWeapon')}</p>
-      <WeaponSkinCard skinCardProps={weaponSkinOdin.data}/>
+    <div className="flex flex-col">
+      <h1>Página del arma: {weapon}</h1>
+      <p>UUID: {searchParams.get("idWeapon")}</p>
+      {weaponSkins && weaponSkins.length > 0 ? (
+        weaponSkins.map((skin, key) => (
+          <WeaponSkinCard skinCardProps={skin} key={key} />
+        ))
+      ) : (
+        <p>No skins available.</p>
+      )}
     </div>
   );
 }
