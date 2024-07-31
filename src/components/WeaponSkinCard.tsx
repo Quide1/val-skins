6d3a410c664324e8c38509de1edebc419d2314ce
Quide1 <ezequielr.dev@gmail.com>
2024-07-30 21:06:38 -0300

    change ident

--- src/components/WeaponSkinCard.tsx
+++ src/components/WeaponSkinCard.tsx
@@ -1,53 +1,71 @@
-  import { type Skin } from "@/types/weaponType";
-  import { useState } from "react";
-  type PropsWeaponSkinCard = {
-    skinCardProps: Skin;
+import { useSkinCardInfo } from "@/hooks/useSkinCardInfo";
+import { Chroma, type Skin } from "@/types/weaponType";
+import { normalizedChromaName } from "@/utils/normalizedChromaName";
+
+type PropsWeaponSkinCard = {
+  skinCardProps: Skin;
+};
+
+function WeaponSkinCard({ skinCardProps }: PropsWeaponSkinCard) {
+  const { chromas, displayName } = skinCardProps;
+  const { currentChromaImg, currentChromaName, changeCurrentChroma,currentChromaUuid } =
+    useSkinCardInfo(chromas);
+  const clickHandler = (chroma: Chroma) => {
+    const { displayName, fullRender, uuid } = chroma;
+    const variant = normalizedChromaName(displayName);
+    changeCurrentChroma({
+      chromaImg: fullRender,
+      chromaName: variant,
+      chromaUuid: uuid,
+    });
   };
-  function WeaponSkinCard({ skinCardProps }: PropsWeaponSkinCard) {
-    console.log(skinCardProps);
-    return (
-      <article className="border-2 border-gray-500 bg-slate-900 min-w-[270px] p-2 flex  justify-center flex-col gap-4 max-w-[550px] text-center rounded-sm">
-        <div className="border-2 text-xl">
-          <h2>{skinCardProps.displayName}</h2>
-        </div>
-        <div className="border-2 h-7">
-        
-          {skinCardProps.chromas.length >= 2 ? (<p className="text-red-700 font-bold">ss</p> ): null}
-        </div>
-        <div>
-          <img
-            src={`${skinCardProps.displayIcon}`}
-            alt={`skin ${skinCardProps.displayName}`}
-          />
-        </div>
-        <div className="flex flex-row justify-around min-h-8 border items-center">
-          {skinCardProps.chromas.length >= 2 
-            ? skinCardProps.chromas.map((chroma, index) =>
+
+  return (
+    <article className="border-2 border-gray-500 bg-slate-900 min-w-[270px] p-2 flex justify-center flex-col gap-4 max-w-[550px] text-center rounded-sm">
+      <div className="border-2 text-xl">
+        <h2 className="font-bold">{displayName}</h2>
+      </div>
+      <div className="border-2 h-7">
+        { (
+          <p className="text-red-700 font-bold">{currentChromaName}</p>
+        ) }
+      </div>
+      <div>
+        <img
+          src={`${currentChromaImg}`}
+          alt={`skin ${skinCardProps.displayName}`}
+        />
+      </div>
+      <div className="flex flex-row justify-around min-h-8 border items-center">
+        { skinCardProps.chromas.map((chroma) =>
               chroma?.swatch ? (
-                <div className="border border-red-white hover:scale-125 animate-in transition-all">
+                <div
+                  className={`border border-red-white hover:scale-125 animate-in transition-all ${currentChromaUuid === chroma.uuid ? 'border-2 border-red-600' : null}` }
+                  key={chroma.uuid}
+                >
                   <img
                     className="w-6"
-                    key={chroma.uuid}
-                    alt={`chroma ${index}`}
+                    alt={`chroma ${chroma.displayName}`}
                     src={chroma.swatch}
+                    onClick={() => clickHandler(chroma)}
                   ></img>
                 </div>
               ) : null
             )
-            : null}
+          }
+      </div>
+      <div className="border flex flex-col border-red-500 gap-2">
+        <div className="flex justify-start">
+          <p>Levels</p>
         </div>
-        <div className="border flex flex-col border-red-500 gap-2">
-          <div className="flex justify-start">
-            <p>Levels</p>
-          </div>
-          <div className="flex justify-around text-sm">
-            <p>Vfx</p>
-            <p>Finisher</p>
-            <p>Nose</p>
-          </div>
+        <div className="flex justify-around text-sm">
+          <p>Vfx</p>
+          <p>Finisher</p>
+          <p>Nose</p>
         </div>
-      </article>
-    );
-  }
+      </div>
+    </article>
+  );
+}
 
-  export default WeaponSkinCard;
+export default WeaponSkinCard;
