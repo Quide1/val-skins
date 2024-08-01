0216c25ef42e3f7227e1500b44514005a73eceb5
Quide1 <ezequielr.dev@gmail.com>
2024-07-31 21:57:50 -0300

    change log

--- src/services/weapons.ts
+++ src/services/weapons.ts
@@ -9,6 +9,7 @@ export async function getAllWeapons() {
                 "Content-Type": "application/json",
             },
         });
+        console.log(import.meta.env.VITE_PUBLIC_URL_API)
         const fetchResponse = await response.json();
         return fetchResponse;
     } catch (error) {
