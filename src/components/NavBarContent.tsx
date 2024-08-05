6427a56dddff8fbda96b8c0e453beec571c35639
Quide1 <ezequielr.dev@gmail.com>
2024-08-05 18:22:47 -0300

    add new redirects to page for cards and sprays

--- src/components/NavBarContent.tsx
+++ src/components/NavBarContent.tsx
@@ -4,6 +4,7 @@ import { weaponsItems } from "@/data/weapons";
 import { InterfaceWeaponList } from "@/types/weaponType";
 import NavBarHeader from "./NavBarItemHeader";
 import NavBarAccordion from "./NavBarAccordion";
+import { Link } from "react-router-dom";
 
 function NavBarContent() {
   const [categories, setCategories] = useState<{
@@ -37,12 +38,17 @@ function NavBarContent() {
       <li>
         <NavBarHeader textParagraph="Collectibles" />
       </li>
+      <Link to={"/player-cards"}>
       <li className=" font-medium">
         <h3 className="mt-4">Player cards</h3>
       </li>
+      </Link>
+      <Link to={"/sprays"}>
       <li className=" font-medium">
         <h3 className="mt-4">Sprays</h3>
       </li>
+      </Link>
+     
     </>
   );
 }
