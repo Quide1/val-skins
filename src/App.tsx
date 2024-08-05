24dcef09c39d5a9bed37e606b5645cc4fc6d0ef6
Quide1 <ezequielr.dev@gmail.com>
2024-08-05 02:04:25 -0300

    change flex style

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import NavBarMobile from "./components/NavBarMobile";
 function App() {
   return (
     <>
-      <main className="flex flex-col lg:grid lg:grid-cols-[260px_minmax(0,1fr)] w-full bg-slate-800 text-white">
+      <main className="flex flex-col lg:grid lg:grid-cols-[260px_minmax(0,1fr)] w-full bg-slate-900 text-white">
         <NavBarMobile />
         <section className=" lg:col-span-1 flex flex-col fixed h-screen top-0 ">
           <NavBarDesktop />
         </section>
-        <section className="lg:col-start-2 min-h-screen w-full bg-slate-800 flex flex-col">
+        <section className="lg:col-start-2 min-h-screen w-full bg-slate-900 flex flex-col">
           <section className="min-h-screen p-4">{<Outlet />}</section>
           <Footer />
         </section>
