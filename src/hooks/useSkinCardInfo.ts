15ffb54f24325179c0fc0b554398e7f2741ce58e
Quide1 <ezequielr.dev@gmail.com>
2024-08-05 02:05:45 -0300

    Change state

--- src/hooks/useSkinCardInfo.ts
+++ src/hooks/useSkinCardInfo.ts
@@ -2,8 +2,15 @@ import { Chroma } from "@/types/weaponType";
 import { useState } from "react";
 
 export const useSkinCardInfo = (chromas: Chroma[]) => {
-  const [chroma, setChroma] = useState(chromas[0]);
+  const [chroma, setChroma] = useState({
+    ...chromas[0],
+    displayName: "",
+  });
   const changeChroma = (index: number) => {
+    if (index == 0) {
+      setChroma({ ...chromas[index], displayName: "" });
+      return;
+    }
     setChroma(chromas[index]);
   };
 
