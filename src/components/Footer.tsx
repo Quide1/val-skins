e94ad8792c97a9739c943b4e604c82a7fd7db4a7
Quide1 <ezequielr.dev@gmail.com>
2024-08-05 02:05:15 -0300

    nothing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import LinkedInIcon from '@/icons/LinkedinIcon'
 
 function Footer() {
     return (
-        <footer className=' w-full h-[4vh] flex flex-row items-center justify-center gap-2 p-2 bg-slate-800 text-white border-red-600 sm:text-sm'>
+        <footer className=' w-full h-[4vh] flex flex-row items-center justify-center gap-2 p-2 bg-slate-900 text-white border-red-600 sm:text-sm'>
             <p>Hecho por Eze ❤️</p>
             <a aria-label='Conóceme visitando mi linkedin' target={'_blank'} href="https://www.linkedin.com/in/ezequiel-r/" >
                 <LinkedInIcon />
