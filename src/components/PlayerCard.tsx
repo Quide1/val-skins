f7d65baddb130933376c92bcd5df5c8bcaed0131
Quide1 <ezequielr.dev@gmail.com>
2024-08-07 15:15:54 -0300

    create player card

---
+++ src/components/PlayerCard.tsx
@@ -0,0 +1,24 @@
+import { type Datum } from "@/types/playerCardType";
+import { Button } from "./ui/button";
+type PlayerCardProps = {
+  props: Datum;
+};
+function PlayerCard({ props }: PlayerCardProps) {
+  return (
+    <article className="bg-slate-950 flex flex-col items-center justify-center gap-2 p-4 rounded-lg border-2 border-slate-950 hover:border-red-700 transition-all">
+      <h3 className=" font-bold text-xl">{props.displayName}</h3>
+      <div className="max-w-[200px]">
+        <img
+          src={`${props.largeArt}`}
+          alt={`banner of ${props.displayName}`}
+          className="w-full"
+        />
+      </div>
+      <Button className="bg-red-700" variant={"ghost"}>
+        Select
+      </Button>
+    </article>
+  );
+}
+
+export default PlayerCard;
