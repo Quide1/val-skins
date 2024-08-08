672434fa18d2f0ee2e1354d71ba669539b743b29
Quide1 <ezequielr.dev@gmail.com>
2024-08-08 18:30:48 -0300

    create player card with onclick

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,9 +1,14 @@
 import { type Datum } from "@/types/playerCardType";
 import { Button } from "./ui/button";
+
 type PlayerCardProps = {
   props: Datum;
+  createChangeHandler: (newInfo: Datum) => void
 };
-function PlayerCard({ props }: PlayerCardProps) {
+
+
+function PlayerCard({ props, createChangeHandler }: PlayerCardProps) {
+  const clickHandler = createChangeHandler()
   return (
     <article className="bg-slate-950 flex flex-col items-center justify-center gap-2 p-4 rounded-lg border-2 border-slate-950 hover:border-red-700 transition-all">
       <h3 className=" font-bold text-xl">{props.displayName}</h3>
@@ -14,7 +19,7 @@ function PlayerCard({ props }: PlayerCardProps) {
           className="w-full"
         />
       </div>
-      <Button className="bg-red-700" variant={"ghost"}>
+      <Button className="bg-red-700" variant={"ghost"} onClick={}>
         Select
       </Button>
     </article>
