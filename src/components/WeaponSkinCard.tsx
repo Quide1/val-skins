798e2fd77b7a1007d4f57973df5256429d51294b
Quide1 <ezequielr.dev@gmail.com>
2024-08-09 22:06:52 -0300

    change class

--- src/components/WeaponSkinCard.tsx
+++ src/components/WeaponSkinCard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { useSkinCardInfo } from "@/hooks/useSkinCardInfo";
 import { type Skin } from "@/types/weaponType";
 import { normalizedChromaName } from "@/utils/normalizedChromaName";
@@ -14,15 +14,13 @@ function WeaponSkinCard({ skinCardProps }: PropsWeaponSkinCard) {
   const { chroma, changeChroma } = useSkinCardInfo(chromas);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    console.log(chroma);
-  }, [chroma]);
+  
   const handleImageLoad = () => {
     setIsLoading(false);
   };
 
   return (
-    <article className="  bg-slate-950 min-w-[270px] p-2 flex justify-center flex-col max-w-[350px] text-center rounded-sm items-center border-2 border-slate-900 hover:border-white transition-all gap-2">
+    <article className="  bg-slate-950 w-[270px] p-2 flex justify-center flex-col max-w-[350px] text-center rounded-sm items-center border-2 border-slate-900 hover:border-white transition-all gap-2 ">
       <div className="text-xl">
         <h2 className="font-bold">{displayName}</h2>
       </div>
@@ -38,7 +36,7 @@ function WeaponSkinCard({ skinCardProps }: PropsWeaponSkinCard) {
           alt={`skin ${skinCardProps.displayName}`}
           onLoad={handleImageLoad}
           loading="lazy"
-          className="max-h-full border-white-600"
+          className="max-h-full border-white-600 w-full"
         />
       </div>
       <div className=" flex flex-col gap-2 w-full">
@@ -52,7 +50,7 @@ function WeaponSkinCard({ skinCardProps }: PropsWeaponSkinCard) {
                 className={` bg-gray-600 hover:scale-125 animate-in transition-all ${
                   chroma.uuid === _chroma.uuid ? "border-2 border-red-600" : ""
                 }`}
-                key={_chroma.uuid}
+                key={index}
               >
                 <img
                   className="w-6 h-6 shadow-md shadow-red-400"
@@ -62,7 +60,7 @@ function WeaponSkinCard({ skinCardProps }: PropsWeaponSkinCard) {
                 ></img>
               </div>
             ) : (
-              <p className="text-gray-700"> No chromas available</p>
+              <p className="text-gray-700" key={index}> No chromas available</p>
             )
           )}
         </div>
@@ -84,7 +82,6 @@ function WeaponSkinCard({ skinCardProps }: PropsWeaponSkinCard) {
                   {" "}
                   <p
                     className="text-red-700 font-semibold hover:text-white"
-                    key={level.uuid}
                   >
                     {normalizedLevelName(level.levelItem)}
                   </p>
