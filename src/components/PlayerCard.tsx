b14b34eca381a1b1b42b33379a4f2d6f229f35f5
Quide1 <ezequielr.dev@gmail.com>
2024-08-09 22:05:03 -0300

    add button click handler

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -3,14 +3,15 @@ import { Button } from "./ui/button";
 
 type PlayerCardProps = {
   props: Datum;
-  createChangeHandler: (newInfo: Datum) => void
+  createChangeHandler: (newInfo: Datum) => void;
 };
 
-
 function PlayerCard({ props, createChangeHandler }: PlayerCardProps) {
-  const clickHandler = createChangeHandler()
+  const onClickHandler = () => {
+    createChangeHandler({ ...props });
+  };
   return (
-    <article className="bg-slate-950 flex flex-col items-center justify-center gap-2 p-4 rounded-lg border-2 border-slate-950 hover:border-red-700 transition-all">
+    <article className="bg-slate-950 flex flex-col items-center justify-between gap-2 p-4 rounded-lg border-2 border-slate-950 hover:border-red-700 transition-all w-[236px] h-[620px] ">
       <h3 className=" font-bold text-xl">{props.displayName}</h3>
       <div className="max-w-[200px]">
         <img
@@ -19,9 +20,15 @@ function PlayerCard({ props, createChangeHandler }: PlayerCardProps) {
           className="w-full"
         />
       </div>
-      <Button className="bg-red-700" variant={"ghost"} onClick={}>
-        Select
-      </Button>
+      <a href="#in-game-view">
+        <Button
+          className="bg-red-700"
+          variant={"ghost"}
+          onClick={onClickHandler}
+        >
+          Select
+        </Button>
+      </a>
     </article>
   );
 }
