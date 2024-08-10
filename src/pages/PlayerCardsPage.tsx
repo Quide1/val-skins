a2234160e1c8d46d8c311f730fb4f7412811d78c
Quide1 <ezequielr.dev@gmail.com>
2024-08-09 22:07:36 -0300

    player card

--- src/pages/PlayerCardsPage.tsx
+++ src/pages/PlayerCardsPage.tsx
@@ -7,14 +7,14 @@ import HorizontalCardInGame from "@/components/HorizontalCardInGame";
 import { useInGameCardInfo } from "@/hooks/useInGameCardInfo";
 import { getAllPlayerCards } from "@/services/playerCards";
 import { usePlayerCards } from "@/hooks/usePlayerCards";
-import { Card, type Datum } from "@/types/playerCardType";
+import { type Card } from "@/types/playerCardType";
 import { useNavigate } from "react-router-dom";
+import SearchCard from "@/components/SearchCard";
 const PlayerCard = lazy(() => import("@/components/PlayerCard"));
-
 function PlayerCardsPage() {
   const navigate = useNavigate();
   const { cardInfo, changeCardInfo } = useInGameCardInfo();
-  const { allCards, allCardsRef, setAllCards, createNewRef } = usePlayerCards();
+  const { allCards, setAllCards, createNewRef, searchSkinByName } = usePlayerCards();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,17 +30,13 @@ function PlayerCardsPage() {
     fetchData();
   }, []);
 
-  const createChangeHandler = (newInfo:Datum) =>{
-    changeCardInfo(newInfo)
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     console.log(cardInfo)
-  },[cardInfo])
+  }, [cardInfo])
   return (
     <section className="flex flex-col items-center p-4">
       <h1>Cards</h1>
-      <h3>In game view</h3>
+      <h3 id="in-game-view">In game view</h3>
       <section className="flex flex-row flex-wrap items-center justify-center gap-4 border-2 border-red-700 w-full">
         <IconInGame urlIconImage={cardInfo.displayIcon} />
         <BannerArtInGame urlArtImage={cardInfo.largeArt} />
@@ -49,10 +45,13 @@ function PlayerCardsPage() {
       <div>
         <PlayerOnline />
       </div>
+      <div >
+        <SearchCard searchCardByName={searchSkinByName} />
+      </div>
       <div className="flex flex-row flex-wrap gap-10 p-4 items-center justify-around bg-slate-900 ">
         {allCards?.map((e) => (
-          <Suspense fallback={<Loader />}>
-            <PlayerCard props={e} key={e.uuid}  createChangeHandler={createChangeHandler(e)}/>
+          <Suspense fallback={<Loader />} key={e.uuid}>
+            <PlayerCard props={e} createChangeHandler={changeCardInfo} />
           </Suspense>
         ))}
       </div>
