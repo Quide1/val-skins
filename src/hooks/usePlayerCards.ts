dc3b0ce273bb90f5cf6df47787d09b10efe89904
Quide1 <ezequielr.dev@gmail.com>
2024-08-09 22:07:07 -0300

    change hook

--- src/hooks/usePlayerCards.ts
+++ src/hooks/usePlayerCards.ts
@@ -2,16 +2,26 @@ import { useRef, useState } from "react";
 import { type Datum } from "@/types/playerCardType";
 
 export function usePlayerCards() {
-  const [allCards,setAllCards]=useState<Datum[]>()
-  const allCardsRef = useRef<Datum[] | null>();
-  
-  const createNewRef = (newUserCards:Datum[])=>{
-    allCardsRef.current =newUserCards
+  const [allCards, setAllCards] = useState<Datum[]|null>(null);
+  const allCardsRef = useRef<Datum[] | null>(null);
+
+  const createNewRef = (newUserCards: Datum[]) => {
+    allCardsRef.current = newUserCards;
+  };
+
+  function searchSkinByName(words: string) {
+    const newCards = allCardsRef.current?.filter((i) =>
+      i.displayName.match(new RegExp(words, "i"))
+    );
+    newCards ? setAllCards(newCards) : null;
   }
+
   return {
     allCardsRef,
     allCards,
     setAllCards,
-    createNewRef
+    createNewRef,
+    searchSkinByName,
+    
   };
 }
