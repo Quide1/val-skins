d5b935efc962c484524e1401f848f727a3e34565
Quide1 <ezequielr.dev@gmail.com>
2024-08-14 21:42:10 -0300

    change icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,6 @@ function Footer() {
     return (
         <footer className=' w-full h-[4vh] flex flex-row items-center justify-center gap-2 p-2 bg-slate-900 text-white border-red-600 sm:text-sm'>
             <p>Hecho por Eze ❤️</p>
-            <a aria-label='Conóceme visitando mi linkedin' target={'_blank'} href="https://www.linkedin.com/in/ezequiel-r/" >
-                <LinkedInIcon />
-            </a>
             <a aria-label='Conoce mas sobre mis proyectos en github' target={'_blank'} href="https://github.com/Quide1" >
                 <GitHubIcon />
             </a>
