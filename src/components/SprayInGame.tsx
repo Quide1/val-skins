bb13de2cc407a131678b96287d550104dd18b6c5
Quide1 <ezequielr.dev@gmail.com>
2024-08-14 21:42:45 -0300

    change style

--- src/components/SprayInGame.tsx
+++ src/components/SprayInGame.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { type Datum } from '@/types/spraysType'
 
 type ComponentProps = {
@@ -8,11 +7,11 @@ type ComponentProps = {
 function SprayInGame({ sprayProps }: ComponentProps) {
     const renderImage = sprayProps?.animationGif || sprayProps?.fullTransparentIcon || sprayProps?.displayIcon || undefined;
     return (
-        <div className='flex flex-col gap-2 items-center justify-center border-2 border-red-700 w-[300px] h-[320px]'>
-            <p className='font-bold text-xl'>{sprayProps.displayName}</p>
+        <div className='flex flex-col gap-2 items-center justify-center border-2 border-red-700 w-[300px] h-[320px] bg-slate-950 rounded-lg'>
+            <p className='font-bold text-xl text-center p-1'>{sprayProps.displayName}</p>
             <div className=' w-[250px] h-[250px] flex items-center justify-center flex-col'>
                 {renderImage ? (
-                    <img src={renderImage} alt="spray in game" />
+                    <img src={renderImage} alt="spray in game" loading='lazy'/>
                 ) : (
                     <p>No image available</p>
                 )}
