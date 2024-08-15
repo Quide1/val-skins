8c2f1eccebdce8d0e6d6ef53e3b5eb7728a6d827
Quide1 <ezequielr.dev@gmail.com>
2024-08-14 21:42:32 -0300

    change styles

--- src/components/SearchSkin.tsx
+++ src/components/SearchSkin.tsx
@@ -4,18 +4,17 @@ type PropsComponent = {
 
 function searchSkin({ searchSkinByName }: PropsComponent) {
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-
     searchSkinByName(event.target.value)
   }
   return (
-    <div className="sticky top-0  w-full bg-transparent flex items-center justify-center">
-    <input
-      type="text"
-      className="w-full border-2 rounded-lg  border-red-700 p-2 m-2 text-black max-w-64"
-      placeholder="Prime vandal"
-      onChange={onChangeHandler}
-    />
-  </div>
+    <div className="sticky top-0  w-full bg-slate-900 flex items-center justify-center ">
+      <input
+        type="text"
+        className="w-full border-2 rounded-lg  border-red-700 p-2 m-2 text-black max-w-64"
+        placeholder="prime vandal"
+        onChange={onChangeHandler}
+      />
+    </div>
   );
 }
 
