27e5cd72930cd48cb0cd494720007fcef8025157
Quide1 <ezequielr.dev@gmail.com>
2024-08-14 21:43:36 -0300

    change indentation

--- src/pages/SpraysPage.tsx
+++ src/pages/SpraysPage.tsx
@@ -1,26 +1,25 @@
-import { useState, lazy, useEffect, useRef } from "react";
+import { useState, lazy, useEffect, useRef, Suspense } from "react";
 
 import { type Spray, Datum } from "@/types/spraysType";
 import { getAllSprays } from "@/services/sprays";
 import { SearchSpray } from "@/components/SearchSpray";
-const SprayInGame = lazy(() => import("@/components/SprayInGame"))
+import Loader from "@/components/Loader";
+const SprayInGame = lazy(() => import("@/components/SprayInGame"));
 function SpraysPage() {
-  const sprayRef = useRef<Datum[]|null>(null)
-  const [sprays, setSprays] = useState<null | Datum[]>(null)
+  const sprayRef = useRef<Datum[] | null>(null);
+  const [sprays, setSprays] = useState<null | Datum[]>(null);
   const fetchData = async () => {
     const response = (await getAllSprays()) as Spray;
     if (response && response.data) {
       setSprays(response.data);
-      sprayRef.current = response.data
-      console.log(response)
+      sprayRef.current = response.data;
     } else {
       console.error("No skins found for weapon.");
     }
   };
   useEffect(() => {
-    fetchData()
-  }, [])
-
+    fetchData();
+  }, []);
 
   function searchSkinByName(words: string) {
     const newSprays = sprayRef.current?.filter((i) =>
@@ -31,16 +30,20 @@ function SpraysPage() {
 
   return (
     <section className="flex flex-row flex-wrap items-center justify-center p-4 gap-4 border-2">
-      <SearchSpray  searchSprayByName={searchSkinByName}/>
-      {
-        sprays && sprays.length > 0 ?sprays?.map((e) => {
-          console.log(e)
-          return (<SprayInGame  key={e.uuid} sprayProps={e} />)
-        }) : (<p>No sprays available</p>)
-        
-      }
+      <SearchSpray searchSprayByName={searchSkinByName} />
+      {sprays && sprays.length > 0 ? (
+        sprays?.map((e) => {
+          return (
+            <Suspense fallback={<Loader />} key={e.uuid} >
+              <SprayInGame sprayProps={e} />
+            </Suspense>
+          );
+        })
+      ) : (
+        <p>No sprays available</p>
+      )}
     </section>
-  )
+  );
 }
 
-export default SpraysPage
+export default SpraysPage;
